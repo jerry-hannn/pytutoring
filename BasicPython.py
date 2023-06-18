6c8e3e227556904c448637fc201d50cd84f670e7
Jerry Han <109703519+jerry-hannn@users.noreply.github.com>
2023-06-17 20:42:19 -0500

    Update BasicPython.py

--- BasicPython.py
+++ BasicPython.py
@@ -1,17 +1,3 @@
-pi = 3.14159
-radius = 2.2
-# area of circle equation <- this is a comment
-area = pi*(radius**2)
-print(area)
-
-# change values of radius <- another comment
-# use comments to help others understand what you are doing in code
-radius = radius + 1
-print(area)     # area doesn't change
-area = pi*(radius**2)
-print(area)
-
-
 #############################
 #### COMMENTING LINES #######
 #############################
@@ -26,6 +12,43 @@ print(area)
 #print(area)
 
 
+# try using type()!
+# print(type(3))
+# print(type(3.0))
+
+# try casting!
+# print(float(3))
+# print(int(3.9))
+
+# simple operations
+# print(1+2)
+# print(3-1)
+# print(1*2)
+# print(4/2)
+# print(5/2)
+# print(5//2)
+# print(5%2)
+# print(2**3)
+
+# order of operations
+# print(1+2*3)
+# print(1+(2*3))
+# print((1+2)*3)
+
 # define a variable
 a_very_long_variable_name_dont_name_them_this_long_pls = 0
+
+pi = 3.14159
+radius = 2.2
+# area of circle equation <- this is a comment
+area = pi*(radius**2)
+print(area)
+
+# change values of radius <- another comment
+# use comments to help others understand what you are doing in code
+radius = radius + 1
+print(area)     # area doesn't change
+area = pi*(radius**2)
+print(area)
+
 
