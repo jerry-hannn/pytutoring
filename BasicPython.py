9b679035cf004ba54769668a67d48d90af8ead07
Jerry Han <109703519+jerry-hannn@users.noreply.github.com>
2023-06-17 19:43:07 -0500

    Basic Python and Simple Tetris

---
+++ BasicPython.py
@@ -0,0 +1,31 @@
+pi = 3.14159
+radius = 2.2
+# area of circle equation <- this is a comment
+area = pi*(radius**2)
+print(area)
+
+# change values of radius <- another comment
+# use comments to help others understand what you are doing in code
+radius = radius + 1
+print(area)     # area doesn't change
+area = pi*(radius**2)
+print(area)
+
+
+#############################
+#### COMMENTING LINES #######
+#############################
+# to comment MANY lines at a time, highlight all of them then CTRL+1
+# do CTRL+1 again to uncomment them
+# try it on the next few lines below!
+
+#area = pi*(radius**2)
+#print(area)
+#radius = radius + 1
+#area = pi*(radius**2)
+#print(area)
+
+
+# define a variable
+a_very_long_variable_name_dont_name_them_this_long_pls = 0
+
