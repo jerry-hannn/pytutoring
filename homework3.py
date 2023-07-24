4c605f0b6710b8587f939102d0e88cc62ded2109
Jerry Han <109703519+jerry-hannn@users.noreply.github.com>
2023-07-23 21:38:21 -0500

    add hw4 to commit

--- homework3.py
+++ homework3.py
@@ -34,9 +34,8 @@
 ########
 # Challenge!
 # Write a program to guess the number between 0 and 100 the user is thinking of using bisection search.
-# Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.
+# Enter 'h' to indicate the number is higher. Enter 'l' to indicate the number is lower. Enter 'c' to indicate I guessed correctly.
 # Example:
-#   Input: 50
 #   Output: Is your secret number 50?
 #   Input: h
 #   Output: Is your secret number 25?
@@ -49,3 +48,21 @@
 #   Input: c
 #   Output: I guessed the number!
 ########
+
+max = 100
+min = 0
+guess = 50
+print("Is your secret number 50?")
+while True:
+    x = input()
+    if x == "h":
+        min = guess
+        guess = min + (max - min) // 2
+        print("Is your secret number " + str(guess) + "?")
+    elif x == "l":
+        max = guess
+        guess = min + (max - min) // 2
+        print("Is your secret number " + str(guess) + "?")
+    elif x == "c":
+        print("I guessed the number!")
+        break
