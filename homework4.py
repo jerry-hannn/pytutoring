5ad54662508a33b076994682eeb26112026c3b3a
Jerry Han <109703519+jerry-hannn@users.noreply.github.com>
2023-08-06 15:29:11 -0500

    Add hw9, change hw4

--- homework4.py
+++ homework4.py
@@ -1,3 +1,14 @@
 #########
 # Write a python program to create all possible strings using the letters 'a', 'e', 'i', 'o', 'u'. Each character should only be used once.
 #########
+# import itertools
+# [print(''.join(i)) for i in itertools.permutations('aeiou')]
+
+# Bruteforce
+# for i in range(0, 5):
+#     for j in range(0, 5):
+#         for k in range(0, 5):
+#             for l in range(0, 5):
+#                 for m in range(0, 5):
+#                     if (i != j and i != k and i != l and i != m and j != k and j != l and j != m and k != l and k != m and l != m):
+#                         print('aeiou'[i] + 'aeiou'[j] + 'aeiou'[k] + 'aeiou'[l] + 'aeiou'[m])
---
+++ homework9.py
@@ -0,0 +1,14 @@
+#############
+# Write a function that multiplies all the items in a list by 2, and returns the sum of the results.
+# Then, call the function with various lists, and print the results.
+#############
+# Your code here
+
+
+
+
+#############
+# Write a function that takes a list of numbers, and returns a list of all the numbers that are divisible by 3.
+# Then, write a function that takes a list of numbers, and prints all the numbers that are divisible by 4.
+# Using these two functions, print all the numbers that are divisible by 12 in a list of numbers.
+#############
